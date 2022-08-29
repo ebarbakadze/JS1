f55554e3f1b044124e328a57416ea24ee1ba5e6c
ebarbakadze <barbakadzee@gmail.com>
2022-08-29 21:49:33 +0400

    JS1

---
+++ script.js
@@ -0,0 +1,103 @@
+// davaleba 1
+// let user = {
+//     firstname: "giorgi",
+//     lastname: "smith",
+//     age: 25,
+//     studentstatus: "active"
+// };
+
+// console.log(user.studentstatus);
+
+
+// davaleba 2
+// let numbers1 = [5, 6, 10, 25, 3];
+
+// for (let i = 0; i < numbers1.length; i++) {
+//     console.log(numbers1[i])
+// }
+
+
+// let numbers2 = [5, 6, 10, 25, 3];
+
+// let y=0;
+// while (y < numbers2.length) {
+//     console.log(numbers2[y]);
+//     y++;
+// }
+
+
+// davaleba 3
+// let numbers3 = [5, 6, 10, 25, 3, 7, 4, 100, 28, 36, 89];
+// for (let i = 0; i < numbers3.length; i++) {
+//     if (numbers3[i] > 5) {
+//        console.log(numbers3[i]);
+//     }
+// }
+
+// let numbers4 = [5, 6, 10, 25, 3, 7, 4, 100, 28, 36, 89];
+// for (let element of numbers4) {
+//     if (element > 5) {
+//         console.log(element)
+//     }
+// }
+
+
+// davaleba 4
+// let user = {
+//     name: 'giorgi',
+//     age:  20,
+//     studentstatus: 'active'
+// }
+// if (user.age < 18 && user.studentstatus == 'active') {
+//     console.log('Hello');
+// } else if (user.name == 'levani') {
+//     console.log('hello levani');
+// } else if (user.studentstatus == 'active' || user.age < 25) {
+//     console.log('hello world');
+// } else {
+//     console.log('error')
+// }
+
+
+// davaleba 5
+// let array = [ 'watermelon', 'pear', 10, 45, 50, 'apple', 'ananas' ];
+// for (let item of array) {
+//     if (typeof item == 'string') {
+//         console.log(item);
+//     }
+// }
+
+
+
+// davaleba 6 ver gavakete yvela masivze ver gamovitane
+// let array = [ [2, -3, 5, 10], [25, -24, -11, 100] , [-6, -7, 10] ]
+// for (let element of array [0]) {
+//     if (element > 0) {
+//         console.log(element);
+//     }
+// }
+
+
+// davaleba 7.1
+// let array = [ 2, 3, 5, 10,25,24,11, 100 , 6,7, 10 ]
+// for (let item of array) {
+//     if (item % 2 == 0) {
+//         console.log(item);
+//     }
+// }
+
+// davaleba 7.2
+// let array = [ 2, 3, 5, 10,25,24,11, 100 , 6,7, 10 ]
+// for (let item of array) {
+//     if (item % 2 == 1) {
+//         console.log(item);
+//     }
+// }
+
+
+// davaleba 8 ver gavakete
+// let users = [
+//     {username: 'giorgi', status: false},
+//     {username: 'levani', status: false},
+//     {username: 'anna', status: true}
+// ]
